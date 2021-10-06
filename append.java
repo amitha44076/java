58a3e40ddf9f26b8cdad99a2977e9a3c6c579cd3
amitha44076 <76677992+amitha44076@users.noreply.github.com>
2021-10-06 12:00:37 +0530

    Add files via upload
    exam q1

---
+++ append.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+class append
+{
+public static void main(String args[])
+{
+int n3;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter number1");
+int n1=sc.nextInt();
+System.out.println("enter second number");
+int n2=sc.nextInt();
+System.out.println("appended result is:");
+System.out.println(n1+""+n2);
+
+}}
